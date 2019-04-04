e523d55caee6d5a90cf80109855a3e1900e3e6fa
Noah Botimer <botimer@umich.edu>
2019-04-04 17:03:38 -0400

    Move Settings setup from initializer to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Settings = Ettin.for(Ettin.settings_files(Rails.root.join('config'), Rails.env))
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/initializers/1_settings.rb
+++
@@ -1,1 +0,0 @@
-Settings = Ettin.for(Ettin.settings_files(Rails.root.join('config'), Rails.env))
